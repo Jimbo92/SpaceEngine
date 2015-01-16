8144329feec0e6adf32e714e598a5bc8d9ccd91f
James <j.stone92@bigpond.com>
2015-01-16 23:14:25 +1100

    First Commit.

---
+++ SpaceEngine/Game1.cs
@@ -0,0 +1,80 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace SpaceEngine
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        //Global Varibles
+        public static Vector2 _ScreenSize = new Vector2(800, 600);
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+
+            #if WINDOWS
+            graphics.PreferredBackBufferWidth = (int)_ScreenSize.X;
+            graphics.PreferredBackBufferHeight = (int)_ScreenSize.Y;
+            graphics.ApplyChanges();
+            #endif
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            Input.Begin();
+
+            if (Input.KeyboardPressed(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            Input.End();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SpaceEngine/Source/Input.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+    static class Input
+    {
+        static public KeyboardState PreviousKeyboard;
+        static public KeyboardState CurrentKeyboard;
+
+        static public bool KeyboardPressed(Keys key)
+        {
+            return CurrentKeyboard.IsKeyDown(key) & PreviousKeyboard.IsKeyUp(key);
+        }
+        static public bool KeyboardReleased(Keys key)
+        {
+            return CurrentKeyboard.IsKeyUp(key) & PreviousKeyboard.IsKeyDown(key);
+        }
+        static public bool KeyboardPress(Keys key)
+        {
+            return CurrentKeyboard.IsKeyDown(key);
+        }
+        static public bool KeyboardRelease(Keys key)
+        {
+            return CurrentKeyboard.IsKeyUp(key);
+        }
+
+        static public MouseState PreviousMouse;
+        static public MouseState CurrentMouse;
+
+        public enum EClicks
+        {
+            LEFT,
+            MIDDLE,
+            RIGHT
+        }
+
+        static public bool ClickPressed(EClicks click)
+        {
+            switch (click)
+            {
+                case EClicks.LEFT: return CurrentMouse.LeftButton == ButtonState.Pressed & PreviousMouse.LeftButton == ButtonState.Released;
+                case EClicks.MIDDLE: return CurrentMouse.MiddleButton == ButtonState.Pressed & PreviousMouse.MiddleButton == ButtonState.Released;
+                case EClicks.RIGHT: return CurrentMouse.RightButton == ButtonState.Pressed & PreviousMouse.RightButton == ButtonState.Released;
+            }
+            return false;
+        }
+        static public bool ClickPress(EClicks click)
+        {
+            switch (click)
+            {
+                case EClicks.LEFT: return CurrentMouse.LeftButton == ButtonState.Pressed;
+                case EClicks.MIDDLE: return CurrentMouse.MiddleButton == ButtonState.Pressed;
+                case EClicks.RIGHT: return CurrentMouse.RightButton == ButtonState.Pressed;
+            }
+            return false;
+        }
+        static public bool ClickReleased(EClicks click)
+        {
+            switch (click)
+            {
+                case EClicks.LEFT: return CurrentMouse.LeftButton == ButtonState.Released & PreviousMouse.LeftButton == ButtonState.Pressed;
+                case EClicks.MIDDLE: return CurrentMouse.MiddleButton == ButtonState.Released & PreviousMouse.MiddleButton == ButtonState.Pressed;
+                case EClicks.RIGHT: return CurrentMouse.RightButton == ButtonState.Released & PreviousMouse.RightButton == ButtonState.Pressed;
+            }
+            return false;
+        }
+        static public bool ClickRelease(EClicks click)
+        {
+            switch (click)
+            {
+                case EClicks.LEFT: return CurrentMouse.LeftButton == ButtonState.Released;
+                case EClicks.MIDDLE: return CurrentMouse.MiddleButton == ButtonState.Released;
+                case EClicks.RIGHT: return CurrentMouse.RightButton == ButtonState.Released;
+            }
+            return false;
+        }
+
+        static public void Begin()
+        {
+            CurrentKeyboard = Keyboard.GetState();
+            CurrentMouse = Mouse.GetState();
+        }
+
+        static public void End()
+        {
+            PreviousKeyboard = CurrentKeyboard;
+            PreviousMouse = CurrentMouse;
+        }
+    }
